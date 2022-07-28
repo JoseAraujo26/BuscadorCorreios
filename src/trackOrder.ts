fa3569acaf794b1b11b905b8d58744fef16d2909
José Emiliano <araujo_26@yahoo.com>
2022-07-28 13:56:19 -0300

    Correção de logs

--- src/trackOrder.ts
+++ src/trackOrder.ts
@@ -3,10 +3,8 @@ import { deliveryStatus } from './db'
 export const trackOrder = async (codes: string[]) => {
   const response = []
   for (const code of codes) {
-    console.log(code)
     const resp = deliveryStatus.get(code)
     response.push(resp)
   }
-  console.log(response)
   return (await Promise.all([...response])).map(order => order.data)
 }
